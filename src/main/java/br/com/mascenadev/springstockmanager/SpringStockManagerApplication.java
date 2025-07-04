12ca7180b3663d338732669571f19c6030ebc7f4
Gilberto Mascena <gilbertomascena@gmail.com>
2025-07-04 17:07:53 -0300

    :tada: init: project created

---
+++ src/main/java/br/com/mascenadev/springstockmanager/SpringStockManagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.mascenadev.springstockmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStockManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStockManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/mascenadev/springstockmanager/SpringStockManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mascenadev.springstockmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStockManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
