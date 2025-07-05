9884272b275fc11c9909098d769428bd7631a67d
Gilberto Mascena <gilbertomascena@gmail.com>
2025-07-04 21:20:28 -0300

    :sparkles: feat: Created and implemented the MovimentacaoRepository interface

---
+++ src/main/java/br/com/mascenadev/springstockmanager/repository/MovimentacaoRepository.java
@@ -0,0 +1,18 @@
+package br.com.mascenadev.springstockmanager.repository;
+
+import br.com.mascenadev.springstockmanager.model.Movimentacao;
+import br.com.mascenadev.springstockmanager.model.Produto;
+import br.com.mascenadev.springstockmanager.model.enums.TipoMovimentacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface MovimentacaoRepository extends JpaRepository<Movimentacao, Long> {
+
+    List<Movimentacao> findByProduto(Produto produto);
+
+    List<Movimentacao> findByDataHoraBetween(LocalDateTime inicio, LocalDateTime fim);
+
+    List<Movimentacao> findByTipo(TipoMovimentacao tipo);
+}
