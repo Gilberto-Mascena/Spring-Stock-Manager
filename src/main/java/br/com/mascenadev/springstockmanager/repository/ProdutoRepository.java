596c0571193896511a2084084a0acd913202dae0
Gilberto Mascena <gilbertomascena@gmail.com>
2025-07-07 22:29:26 -0300

    :sparkles: feat: Updated ProdutoRepository interface

--- src/main/java/br/com/mascenadev/springstockmanager/repository/ProdutoRepository.java
+++ src/main/java/br/com/mascenadev/springstockmanager/repository/ProdutoRepository.java
@@ -1,6 +1,6 @@
 package br.com.mascenadev.springstockmanager.repository;
 
-import br.com.mascenadev.springstockmanager.model.Produto;
+import br.com.mascenadev.springstockmanager.model.produto.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
