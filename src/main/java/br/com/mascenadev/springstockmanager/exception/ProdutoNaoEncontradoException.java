f133af465dd065182f5a0f02776b0dc6ccbaeb72
Gilberto Mascena <gilbertomascena@gmail.com>
2025-07-09 15:25:05 -0300

    :sparkles: feat: Created and implemented the ProdutoNaoEncontradoException exception

---
+++ src/main/java/br/com/mascenadev/springstockmanager/exception/ProdutoNaoEncontradoException.java
@@ -0,0 +1,16 @@
+package br.com.mascenadev.springstockmanager.exception;
+
+public class ProdutoNaoEncontradoException extends RuntimeException {
+
+    public ProdutoNaoEncontradoException() {
+        super("Produto não encontrado");
+    }
+
+    public ProdutoNaoEncontradoException(Long id) {
+        super("Produto não encontrado com o ID: " + id);
+    }
+
+    public ProdutoNaoEncontradoException(String nome) {
+        super("Produto não encontrado com o nome: " + nome);
+    }
+}
