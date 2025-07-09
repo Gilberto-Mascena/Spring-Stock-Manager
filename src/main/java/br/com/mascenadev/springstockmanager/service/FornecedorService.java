f6471e24c2ef7b2ae4017a48c8ad3f44083b93ae
Gilberto Mascena <gilbertomascena@gmail.com>
2025-07-09 13:51:21 -0300

    :sparkles: feat: Created and implemented the FornecedorService class

---
+++ src/main/java/br/com/mascenadev/springstockmanager/service/FornecedorService.java
@@ -0,0 +1,60 @@
+package br.com.mascenadev.springstockmanager.service;
+
+import br.com.mascenadev.springstockmanager.model.fornecedor.Fornecedor;
+import br.com.mascenadev.springstockmanager.model.fornecedor.dto.FornecedorRequestDTO;
+import br.com.mascenadev.springstockmanager.model.fornecedor.dto.FornecedorResponseDTO;
+import br.com.mascenadev.springstockmanager.repository.FornecedorRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FornecedorService {
+
+    private final FornecedorRepository fornecedorRepository;
+
+    public FornecedorService(FornecedorRepository fornecedorRepository) {
+        this.fornecedorRepository = fornecedorRepository;
+    }
+
+    @Transactional
+    public FornecedorResponseDTO save(FornecedorRequestDTO fornecedorRequestDTO) {
+
+        Fornecedor fornecedor = new Fornecedor();
+        BeanUtils.copyProperties(fornecedorRequestDTO, fornecedor);
+        Fornecedor fornecedorSalvo = fornecedorRepository.save(fornecedor);
+        return new FornecedorResponseDTO(fornecedorSalvo);
+    }
+
+    public List<FornecedorResponseDTO> findAll() {
+        List<Fornecedor> fornecedores = fornecedorRepository.findAll();
+        return fornecedores.stream()
+                .map(FornecedorResponseDTO::new)
+                .toList();
+    }
+
+    public FornecedorResponseDTO findById(Long id) {
+        Fornecedor fornecedor = fornecedorRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Fornecedor não encontrado para o ID: " + id));
+        return new FornecedorResponseDTO(fornecedor);
+    }
+
+    @Transactional
+    public FornecedorResponseDTO update(Long id, FornecedorRequestDTO fornecedorRequestDTO) {
+        Fornecedor fornecedor = fornecedorRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Fornecedor não encontrado para o ID: " + id));
+
+        BeanUtils.copyProperties(fornecedorRequestDTO, fornecedor, "id");
+        Fornecedor fornecedorAtualizado = fornecedorRepository.save(fornecedor);
+        return new FornecedorResponseDTO(fornecedorAtualizado);
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Fornecedor fornecedor = fornecedorRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Fornecedor não encontrado"));
+        fornecedorRepository.delete(fornecedor);
+    }
+}
