402f83183634506f0f8ba988eb68d88db7ff158d
Gilberto Mascena <gilbertomascena@gmail.com>
2025-07-09 15:27:14 -0300

    :sparkles: feat: Created and implemented the GlobalExceptionHandler class

---
+++ src/main/java/br/com/mascenadev/springstockmanager/exception/GlobalExceptionHandler.java
@@ -0,0 +1,76 @@
+package br.com.mascenadev.springstockmanager.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> handleValidationErrorsException(MethodArgumentNotValidException ex) {
+        Map<String, String> fieldError = new HashMap<>();
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            fieldError.put(error.getField(), error.getDefaultMessage());
+        }
+
+        StandardError standardError = new StandardError(
+                LocalDateTime.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                "Erro de validação nos campos enviados",
+                fieldError
+        );
+        return ResponseEntity.badRequest().body(standardError);
+    }
+
+    @ExceptionHandler(ProdutoNaoEncontradoException.class)
+    public ResponseEntity<StandardError> handleProdutoNaoEncontradoException(ProdutoNaoEncontradoException ex) {
+        StandardError standardError = new StandardError(
+                LocalDateTime.now(),
+                HttpStatus.NOT_FOUND.value(),
+                "Produto não encontrado",
+                ex.getMessage()
+        );
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(standardError);
+    }
+
+    @ExceptionHandler(MovimentacaoNaoEncontradaException.class)
+    public ResponseEntity<StandardError> handleMovimentacaoNaoEncontradaException(MovimentacaoNaoEncontradaException ex) {
+        StandardError standardError = new StandardError(
+                LocalDateTime.now(),
+                HttpStatus.NOT_FOUND.value(),
+                "Movimentação não encontrada",
+                ex.getMessage()
+        );
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(standardError);
+    }
+
+    @ExceptionHandler(FornecedorNaoEncontradoException.class)
+    public ResponseEntity<StandardError> handleFornecedorNaoEncontradoException(FornecedorNaoEncontradoException ex) {
+        StandardError standardError = new StandardError(
+                LocalDateTime.now(),
+                HttpStatus.NOT_FOUND.value(),
+                "Fornecedor não encontrado",
+                ex.getMessage()
+        );
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(standardError);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<StandardError> handleGenericException(Exception ex) {
+        StandardError standardError = new StandardError(
+                LocalDateTime.now(),
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "Erro interno do servidor",
+                "Ocorreu um erro inesperado."
+        );
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(standardError);
+    }
+}
