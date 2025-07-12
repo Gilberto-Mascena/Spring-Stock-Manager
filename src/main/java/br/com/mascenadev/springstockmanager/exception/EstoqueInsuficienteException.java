4fcd01e6ff69a12df762ebc325ed88b80ec2ed6a
Gilberto Mascena <gilbertomascena@gmail.com>
2025-07-11 21:29:58 -0300

    :sparkles: feat: Created and implemented the EstoqueInsuficienteException exception

---
+++ src/main/java/br/com/mascenadev/springstockmanager/exception/EstoqueInsuficienteException.java
@@ -0,0 +1,14 @@
+package br.com.mascenadev.springstockmanager.exception;
+
+public class EstoqueInsuficienteException extends RuntimeException {
+    public EstoqueInsuficienteException() {
+        super("Estoque insuficiente para o produto");
+    }
+    public EstoqueInsuficienteException(Long idProduto) {
+        super("Estoque insuficiente para o produto: " + idProduto);
+    }
+
+    public EstoqueInsuficienteException(String nome) {
+        super("Estoque insuficiente para o produto: " + nome);
+    }
+}
