67c8b6df3ed9fa7aa0dfa0dfa287225f4bccc408
Gilberto Mascena <gilbertomascena@gmail.com>
2025-07-13 21:46:24 -0300

    :sparkles: feat: Updated FornecedorController class

--- src/main/java/br/com/mascenadev/springstockmanager/controller/FornecedorController.java
+++ src/main/java/br/com/mascenadev/springstockmanager/controller/FornecedorController.java
@@ -3,6 +3,7 @@ package br.com.mascenadev.springstockmanager.controller;
 import br.com.mascenadev.springstockmanager.model.fornecedor.dto.FornecedorRequestDTO;
 import br.com.mascenadev.springstockmanager.model.fornecedor.dto.FornecedorResponseDTO;
 import br.com.mascenadev.springstockmanager.service.FornecedorService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +20,7 @@ public class FornecedorController {
         this.fornecedorService = fornecedorService;
     }
 
+    @Operation(summary = "Create a new fornecedor")
     @PostMapping("/fornecedores")
     public ResponseEntity<FornecedorResponseDTO> createFornecedor(@RequestBody @Valid FornecedorRequestDTO fornecedorRequestDTO) {
 
@@ -31,21 +33,25 @@ public class FornecedorController {
         return ResponseEntity.created(uri).body(fornecedorSaved);
     }
 
+    @Operation(summary = "Get all fornecedores")
     @GetMapping("/fornecedores")
     public ResponseEntity<List<FornecedorResponseDTO>> getAllFornecedores() {
         return ResponseEntity.ok(fornecedorService.findAll());
     }
 
+    @Operation(summary = "Get a fornecedor by ID")
     @GetMapping("/fornecedores/{id}")
     public ResponseEntity<FornecedorResponseDTO> getFornecedorById(@PathVariable Long id) {
         return ResponseEntity.ok(fornecedorService.findById(id));
     }
 
+    @Operation(summary = "Update a fornecedor by ID")
     @PutMapping("/fornecedores/{id}")
     public ResponseEntity<FornecedorResponseDTO> updateFornecedor(@PathVariable Long id, @RequestBody @Valid FornecedorRequestDTO fornecedorRequestDTO) {
         return ResponseEntity.ok(fornecedorService.update(id, fornecedorRequestDTO));
     }
 
+    @Operation(summary = "Delete a fornecedor by ID")
     @DeleteMapping("/fornecedores/{id}")
     public ResponseEntity<Void> deleteFornecedor(@PathVariable Long id) {
         fornecedorService.delete(id);
