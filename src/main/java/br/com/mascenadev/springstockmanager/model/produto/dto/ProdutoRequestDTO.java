42bacdc3e2b7ac827a9197382c6556c80f219432
Gilberto Mascena <gilbertomascena@gmail.com>
2025-08-03 12:37:39 -0300

    :sparkles: feat: Updated imports of the ProdutoRequestDTO class

--- src/main/java/br/com/mascenadev/springstockmanager/model/produto/dto/ProdutoRequestDTO.java
+++ src/main/java/br/com/mascenadev/springstockmanager/model/produto/dto/ProdutoRequestDTO.java
@@ -1,6 +1,10 @@
 package br.com.mascenadev.springstockmanager.model.produto.dto;
 
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
 
 import java.math.BigDecimal;
 
