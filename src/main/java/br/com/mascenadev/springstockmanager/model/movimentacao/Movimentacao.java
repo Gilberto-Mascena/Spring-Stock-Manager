1b4bfc214815eb3625dcedcb78a56875575a5751
Gilberto Mascena <gilbertomascena@gmail.com>
2025-08-03 12:36:53 -0300

    :sparkles: feat: Updated imports of the Movimentacao class

--- src/main/java/br/com/mascenadev/springstockmanager/model/movimentacao/Movimentacao.java
+++ src/main/java/br/com/mascenadev/springstockmanager/model/movimentacao/Movimentacao.java
@@ -2,7 +2,17 @@ package br.com.mascenadev.springstockmanager.model.movimentacao;
 
 import br.com.mascenadev.springstockmanager.model.enums.TipoMovimentacao;
 import br.com.mascenadev.springstockmanager.model.produto.Produto;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
